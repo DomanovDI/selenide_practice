3676e94fe193c7d954f34c782fe7a45429accbb0
DomanovDI <idalgo13@gmail.com>
2023-12-06 17:02:10 +0300

    change CalendarComponent

--- src/test/java/com/demoqa/pages/components/CalendarComponent.java
+++ src/test/java/com/demoqa/pages/components/CalendarComponent.java
@@ -6,6 +6,6 @@ public class CalendarComponent {
     public void setDate(String day, String month, String year){
         $(".react-datepicker__year-select").selectOptionContainingText(year);
         $(".react-datepicker__month-select").selectOptionContainingText(month);
-        $(".react-datepicker__day--0" + day).click();
+        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
     }
 }
